7d653eead3829b4b9ea0858d068c169fd8323292
Robert-Beckett <robert.beckett@onion.red>
2025-07-20 19:36:02 -0300

    - The book now has an injectable aim node. - The book will now begin with an initial trajectory based on the   location of the aim node relative to itself. - The book will now be affected by gravity while in THROWN mode.

--- Weapons/PongWeapon/State/state_thrown.gd
+++ Weapons/PongWeapon/State/state_thrown.gd
@@ -5,6 +5,7 @@ extends State
 
 var weapon: PongWeapon
 var velocity: float = 0
+var initial_trajectory: Vector2
 
 var exiting: bool = false
 
@@ -21,22 +22,18 @@ func enter() -> void:
 	weapon.collision_mask = 0 # Enemy, Terrain and player
 
 	velocity = velocity * weapon.velocity_mul
+	initial_trajectory = weapon.get_aim_vector()
 
 func exit() -> void:
 	velocity = 0
 	exiting = true
 
-func run_process(delta: float) -> void:
-	pass
-
 func run_physics_process(delta: float) -> void:
-	weapon.apply_force(Vector2(velocity, 0) * delta)
-	velocity -= (velocity * 0.1 * delta)
+	weapon.apply_force(initial_trajectory * velocity * delta)
+	velocity -= (velocity * 0.5 * delta)
 	# Toggle a flag once the weapon exits the players collider
 	# This will let the player catch the weapon on the next collision
 
-func run_integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	pass
-
+@warning_ignore("shadowed_variable")
 func set_weapon(weapon: PongWeapon) -> void:
 	self.weapon = weapon
