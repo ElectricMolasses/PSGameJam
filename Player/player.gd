243b757a9002b119ca3679807adad5ceecb46c5e
Hawkward <trent.d.doyle@gmail.com>
2025-07-21 20:15:47 -0400

    Help Me!

--- Player/player.gd
+++ Player/player.gd
@@ -1,8 +1,10 @@
 extends RigidBody2D
 
 @export var force: int = 50
-@export var max_speed: int = 10
-@export var acceleration: float = 60
+@export var max_speed: int = 600
+@export var acceleration: float = 600 * 50
+@export var jump_acceleration: int = 1000
+var jump_triggered: bool = false
 
 func _ready():
 	pass
@@ -14,17 +16,25 @@ func _physics_process(delta: float) -> void:
 func move_player(delta: float) -> void:
 	var input_direction = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
+		print("right")
 		input_direction.x += acceleration * delta
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.play("move_right")
 	if Input.is_action_pressed("move_left"):
+		print("left")
 		input_direction.x -= acceleration * delta
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.play("move_left")
 	if Input.is_action_just_pressed("jump"):
-		input_direction.y += 10
-		$AnimatedSprite2D.play("jump")
+		print("jump")
+		jump_triggered = true
+	
 	input_direction.x = clamp(input_direction.x, -max_speed, max_speed)
 	
 	if input_direction != Vector2.ZERO:
-		apply_central_impulse(input_direction * force)
+		apply_central_force(input_direction)
+	
+	if jump_triggered: 
+		jump_triggered = false
+		apply_impulse(Vector2(0, -1) * jump_acceleration)
+		$AnimatedSprite2D.play("jump")
